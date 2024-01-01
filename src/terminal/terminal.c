5cc9be5299f0096d33a189447b357970ffcf9d32
Taiki Yamaguchi <taiki.yamaguchi@gmail.com>
2024-01-01 18:28:48 +0700

    Prettify teminal/ files

--- src/terminal/terminal.c
+++ src/terminal/terminal.c
@@ -1,26 +1,28 @@
 #include "terminal.h"
+#include "string/string.h"
 #include <stddef.h>
 #include <stdint.h>
-#include "string/string.h"
 
-uint16_t *video_mem = 0;
+uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
 uint16_t terminal_col = 0;
 
-uint16_t terminal_make_char(char c, char color)
+uint16_t
+terminal_make_char(char c, char color)
 {
     return (color << 8) | c;
 }
 
-void terminal_put_char(int x, int y, char c, char color)
+void
+terminal_put_char(int x, int y, char c, char color)
 {
     video_mem[y * VGA_WIDTH + x] = terminal_make_char(c, color);
 }
 
-void terminal_write_char(char c, char color)
+void
+terminal_write_char(char c, char color)
 {
-    if (c == '\n')
-    {
+    if (c == '\n') {
         terminal_col = 0;
         terminal_row += 1;
         return;
@@ -28,38 +30,37 @@ void terminal_write_char(char c, char color)
 
     terminal_put_char(terminal_col, terminal_row, c, color);
     terminal_col += 1;
-    if (terminal_col >= VGA_WIDTH)
-    {
+    if (terminal_col >= VGA_WIDTH) {
         terminal_col = 0;
         terminal_row += 1;
     }
 }
 
-void terminal_initialize()
+void
+terminal_initialize()
 {
-    video_mem = (uint16_t *)0xB8000;
+    video_mem = (uint16_t*)0xB8000;
     terminal_row = 0;
     terminal_col = 0;
 
-    for (int y = 0; y < VGA_HEIGHT; y++)
-    {
-        for (int x = 0; x < VGA_WIDTH; x++)
-        {
+    for (int y = 0; y < VGA_HEIGHT; y++) {
+        for (int x = 0; x < VGA_WIDTH; x++) {
             terminal_put_char(x, y, ' ', 0);
         }
     }
 }
 
-void print(const char *str)
+void
+print(const char* str)
 {
     size_t len = strlen(str);
-    for (int i = 0; i < len; i++)
-    {
+    for (int i = 0; i < len; i++) {
         terminal_write_char(str[i], 15);
     }
 }
 
-void print_int(int n)
+void
+print_int(int n)
 {
     char buf[32];
     itoa(n, buf, 10);
--- src/terminal/terminal.h
+++ src/terminal/terminal.h
@@ -1,11 +1,11 @@
 #ifndef TERMINAL_H
 #define TERMINAL_H
 
-#define VGA_WIDTH 80
+#define VGA_WIDTH  80
 #define VGA_HEIGHT 20
 
 void terminal_initialize();
-void print(const char *str);
+void print(const char* str);
 void print_int(int n);
 
 #endif
